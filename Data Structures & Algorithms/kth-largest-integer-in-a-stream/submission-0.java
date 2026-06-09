5923423ab4cc65be3ae2fd0e5937def1dfec15ed
Tejarr <39567816+tejarrpaladagu@users.noreply.github.com>
2026-06-09 19:40:24 -0400

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,28 @@
+class KthLargest {
+    public PriorityQueue<Integer>minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+        for(int num:nums){
+            minHeap.offer(num);
+            if(minHeap.size() > k){
+                minHeap.poll();
+            }
+        }
+
+
+        
+    }
+    
+    public int add(int val) {
+        minHeap.offer(val);
+        if(minHeap.size() > k){
+            minHeap.poll();
+        }
+        return minHeap.peek();
+
+        
+    }
+}
